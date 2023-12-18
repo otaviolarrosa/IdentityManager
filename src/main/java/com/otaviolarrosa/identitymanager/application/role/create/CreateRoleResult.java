d80dd1e8b7424a04e2d2a80e2a3bb641340f0151
otaviolarrosa <otaviolarrosa@outlook.com>
2023-12-18 09:52:55 -0300

    Removed Java Code

--- src/main/java/com/otaviolarrosa/identitymanager/application/role/create/CreateRoleResult.java
+++
@@ -1,7 +0,0 @@
-package com.otaviolarrosa.identitymanager.application.role.create;
-
-import com.otaviolarrosa.identitymanager.api.commons.ApiResultBase;
-
-public class CreateRoleResult extends ApiResultBase {
-
-}
